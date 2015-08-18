88c467dd11fce8719e3a8b6f9caad23cb9fa9e90
dfl <dfl@0xdf.de>
2015-08-19 00:23:56 +0200

    Added modelfilters

--- model.py
+++ model.py
@@ -12,16 +12,44 @@ def fill_store(store, data):
         store.append(row if isinstance(row, tuple) else (row,))
 
 
-# money transfer - date, person, amount, description
-transfers = Gtk.ListStore(str, str, float, str)
+class GruppenkasseStore(object):
+    def __init__(self, transfers, expenses, persons, participations):
+        # money transfer - date, person, amount, description
+        self.transfers = Gtk.ListStore(str, str, float, str)
+        fill_store(self.transfers, transfers)
 
-# expenses of an event - date, event, amount, description
-expenses = Gtk.ListStore(str, str, float, str)
+        # expenses of an event - date, event, amount, description
+        self.expenses = Gtk.ListStore(str, str, float, str)
+        fill_store(self.expenses, expenses)
 
-# person names
-persons = Gtk.ListStore(str)
+        # person names
+        self.persons = Gtk.ListStore(str)
+        fill_store(self.persons, persons)
 
-# participations of person to events - event, person
-participations = Gtk.ListStore(str, str)
+        # participations of person to events - event, person
+        self.participations = Gtk.ListStore(str, str)
+        fill_store(self.participations, participations)
+
+    def participants_of(self, event):
+        """ 
+        * Returns a liststore of all participants of an event
+        """
+        def visible_func(model, index, user_data):
+            return model[index][0] == event
+
+        modelfilter = self.participations.filter_new()
+        modelfilter.set_visible_func(visible_func)
+
+    def events_attended(self, person):
+        """ 
+        * Returns a liststore of all events this person attended
+        """
+        def visible_func(model, index, user_data):
+            return model[index][0] == person
+
+        modelfilter = self.participations.filter_new()
+        modelfilter.set_visible_func(visible_func)        
+
+
 
 
--- tests/test_stores.py
+++ tests/test_stores.py
@@ -1,33 +1,19 @@
 #!/usr/bin/python3
 # -*- encoding: utf-8 -*-
 import unittest
-import model, example_data
-from model import fill_store
+import model
+import example_data as ex
+from model import GruppenkasseStore
 """ 
 * Uses the ListStores in model.py and fills them with the example data from example_data.py (which is private, i will supply some random data soon)
 """
 
 class TestListStore(unittest.TestCase):
+    def setUp(self):
+        store = GruppenkasseStore(ex.transfers, ex.expenses, ex.persons, ex.participations)
 
     def test_transfers(self):
-        store = model.transfers
-        data = example_data.transfers
-        fill_store(store, data)
-
-    def test_expenses(self):
-        store = model.expenses
-        data = example_data.expenses
-        fill_store(store, data)
-
-    def test_persons(self):
-        store = model.persons
-        data = example_data.persons
-        fill_store(store, data)
-
-    def test_participations(self):
-        store = model.participations
-        data = example_data.participations
-        fill_store(store, data)
+        ...
 
 
 if __name__ == '__main__':
