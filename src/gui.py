1c7e05f193862a682b968790bc6e04f38591e660
RincewindWizzard <RincewindWizzard@0xdf.de>
2015-08-31 22:54:16 +0200

    Read Only Gui funktioniert

--- src/gui.py
+++ src/gui.py
@@ -2,68 +2,103 @@
 # -*- encoding: utf-8 -*-
 from gi.repository import Gtk
 from datetime import datetime
+from decimal import Decimal
+from model import Person
 
 datefmt = '%d.%m.%y'
 
-class BuilderWrapper(Gtk.Builder):
-    """ Mimics a Python dict to access the widgets in a syntatic sugared way """
-    def __getitem__(self, key):
-        return self.get_object(key)
+def strfmoney(amount: "int cents"):
+    return "{:.2f} €".format(amount / 100).replace(".", ",")
 
-class Sidebar(object):
+def strpmoney(amount):
+    amount = amount.replace("€", "").replace(",", ".")
+    return int(Decimal(amount) * 100)
+
+# this function is used to render ints as money string
+def money_cell_renderer(column, cell, store, index, user_data):
+    return store[index]
+
+
+
+class PersonTab(object):
+    def __init__(self, gui):
+        self.kasse = gui.model
+        builder = gui.builder
+        self.person_listview = builder['person_list']
+        self.person_listview.set_grid_lines(Gtk.TreeViewGridLines.BOTH)
+        self.person_list = Gtk.ListStore(int, str, str, str, str)
+        self.person_listview.set_model(self.person_list)
+        self.person_listview.get_selection().connect('changed', self.person_selected)
+
+        self.payments = builder['payments_store']
+            
+        self.update()
+
+    def update(self):
+        self.person_list.clear()
+        for person in self.kasse.persons:
+            self.person_list.append([person.id, person.name, strfmoney(person.payed), strfmoney(person.expenses), strfmoney(person.balance)])
+
+    def person_selected(self, selection):
+        model, index = selection.get_selected()
+        person_id = model[index][0]
+
+        
+        person = self.kasse.db.query(Person).filter(Person.id == person_id).first()
+        self.payments.clear()
+        for payment in person.payments:
+            self.payments.append([payment.id, datetime.strftime(payment.date, datefmt), strfmoney(payment.amount), payment.description])
+
+
+
+
+class EventTab(object):
     def __init__(self, main_gui, kasse):
         self.kasse = kasse
         self.main_gui = main_gui
 
         builder = main_gui.builder
-
-        self.person_listview = builder['person_list']
         self.event_listview = builder['event_list']
-
-        self.person_list = Gtk.ListStore(str)
-        self.event_list = Gtk.ListStore(str)
-
-        self.person_listview.set_model(self.person_list)
+        self.event_list = Gtk.ListStore(int, str, int, str, str)
         self.event_listview.set_model(self.event_list)
-
-        self.person_listview.get_selection().connect('changed', self.person_selected)
         self.event_listview.get_selection().connect('changed', self.event_selected)
 
+        self.participants = builder['participants_store']
         self.update()
 
     def update(self):
-        self.person_list.clear()
         self.event_list.clear()
 
-        for person in self.kasse.persons:
-            self.person_list.append([person.name])
-
         for event in self.kasse.events:
-            self.event_list.append([event.name])
+            self.event_list.append([event.id, event.name, len(event.participants), strfmoney(event.expense_sum), strfmoney(event.expense_per_participant)])
 
     def event_selected(self, selection):
         model, index = selection.get_selected()
-        event_name = model[index][0]
+        event_name = model[index][1]
         event = self.kasse.event_dict[event_name]
         self.main_gui.on_event_selected(event)
 
-    def person_selected(self, selection):
-        model, index = selection.get_selected()
-        print(model[index][0])
+        self.participants.clear()
+        for person in self.kasse.persons:
+            self.participants.append([person.id, person.name, person in event.participants])
+
+
+
         
 
 class GruppenkasseGui(object):
     glade_file = "./res/gruppenkasse.glade"
     def __init__(self, model):
         self.model = model
-
         self.builder = BuilderWrapper()
         self.builder.add_from_file(GruppenkasseGui.glade_file)
 
-        self.sidebar = Sidebar(self, self.model)
+        self.event_tab = EventTab(self, self.model)
+        self.person_tab = PersonTab(self)
+
         self.builder["main_window"].show_all()
 
-        #self.builder['person_list']
+        self.builder['person_list']
 
         # delete Event
         self.builder['main_window'].connect("delete-event", self.on_main_window_delete_event)
@@ -81,6 +116,11 @@ class GruppenkasseGui(object):
         expense_list.clear()
 
         for expense in event.expenses:
-            expense_list.append([datetime.strftime(expense.date, datefmt), "{:.2f} €".format(expense.amount / 100), str(expense.description)])
+            expense_list.append([expense.id, datetime.strftime(expense.date, datefmt), "{:.2f} €".format(expense.amount / 100), str(expense.description)])
 
+
+class BuilderWrapper(Gtk.Builder):
+    """ Mimics a Python dict to access the widgets in a syntatic sugared way """
+    def __getitem__(self, key):
+        return self.get_object(key)
         
